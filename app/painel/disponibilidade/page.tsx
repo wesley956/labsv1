7dd156ce4877123aa270d2a50caf9d5b3bc9ff3e
wesley956 <aweservicosaw@gmail.com>
2026-07-18 10:19:05 -0300

    add availability page

---
+++ app/painel/disponibilidade/page.tsx
@@ -0,0 +1,6 @@
+import { AvailabilityManager } from "@/components/availability-manager";
+import "./availability.css";
+
+export default function AvailabilityPage() {
+  return <AvailabilityManager />;
+}
