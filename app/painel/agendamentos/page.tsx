4104ebef9a01b6623402853c018373691e1c369b
wesley956 <aweservicosaw@gmail.com>
2026-07-18 10:23:26 -0300

    add functional appointments route

---
+++ app/painel/agendamentos/page.tsx
@@ -0,0 +1,6 @@
+import { AppointmentsManager } from "@/components/appointments-manager";
+import "./appointments.css";
+
+export default function AppointmentsPage() {
+  return <AppointmentsManager />;
+}
