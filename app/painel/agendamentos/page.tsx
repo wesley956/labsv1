fd64e39c3b6c0ce887f64631a88dcf43beaac2e6
wesley956 <aweservicosaw@gmail.com>
2026-07-19 01:58:52 -0300

    feat: load appointments for current business

--- app/painel/agendamentos/page.tsx
+++ app/painel/agendamentos/page.tsx
@@ -1,6 +1,8 @@
 import { AppointmentsManager } from "@/components/appointments-manager";
+import { requireCurrentBusiness } from "@/lib/supabase/current-business";
 import "./appointments.css";
 
-export default function AppointmentsPage() {
-  return <AppointmentsManager />;
+export default async function AppointmentsPage() {
+  const business = await requireCurrentBusiness();
+  return <AppointmentsManager businessId={business.id} />;
 }
