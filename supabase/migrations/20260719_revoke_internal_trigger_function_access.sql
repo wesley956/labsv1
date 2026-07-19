32a7c305ebca11fd677751012499950a8de762df
wesley956 <aweservicosaw@gmail.com>
2026-07-19 06:34:34 -0300

    security: hide internal appointment trigger function

---
+++ supabase/migrations/20260719_revoke_internal_trigger_function_access.sql
@@ -0,0 +1,3 @@
+revoke all on function public.log_appointment_status_change() from public;
+revoke all on function public.log_appointment_status_change() from anon;
+revoke all on function public.log_appointment_status_change() from authenticated;
